89e5f06b2ac4708dacc6dc6c8bb333b7aba4bdf4
Maik Broisler <maikbroislerdevs@gmail.com>
2020-06-08 23:37:10 -0300

    GoStack | Project GoBarber: App Mobile with React Native

---
+++ ios/Pods/Target Support Files/Pods-GoBarber-GoBarberTests/Pods-GoBarber-GoBarberTests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_GoBarber_GoBarberTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GoBarber_GoBarberTestsVersionString[];
+
---
+++ ios/Pods/Target Support Files/Pods-GoBarber/Pods-GoBarber-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoBarber : NSObject
+@end
+@implementation PodsDummy_Pods_GoBarber
+@end
